89d37b410053b1a0eecd42b121963d8147121e18
waleed513 <waleedahmad.aj@gmail.com>
2024-07-02 12:48:46 +0500

    Add files via upload

---
+++ con3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class con3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a==b) {
+        System.out.println("EQUAL");
+        }
+        else {
+            System.out.println("Not EQUAL");
+        }
+        if (a>b){
+        System.out.println("A is grea8ter");
+        }else
+         {
+            System.out.println("B is greater");
+         }
+            
+        }
+    }
+
