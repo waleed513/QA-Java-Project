069d5b6551dfb45e592e8ee0990a1c554f43d14b
waleed513 <waleedahmad.aj@gmail.com>
2024-07-03 15:16:06 +0500

    Add files via upload

---
+++ break1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class break1 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0;i<9;i++) {
+            System.out.println(i);
+            System.out.println("JAva is great");
+            if (i==5);{
+            System.out.println("Loop end here");
+            break;
+        }
+    }
+}
+
+}
