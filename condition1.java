6b52a63083fd05fe1c8d03076f915d982c473ea2
waleed513 <waleedahmad.aj@gmail.com>
2024-07-03 14:43:17 +0500

    Add files via upload

---
+++ condition1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class condition1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+    if (x%2==0) 
+    {
+     System.out.println("even number"); }
+     else{
+        System.out.println("odd number");
+     
+     }
+}
+}
---
+++ conditions3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class conditions3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a==b) {
+        System.out.println("EQUAL");
+        }
+        else {
+            System.out.println("Not EQUAL");
+        }
+        if (a>b){
+        System.out.println("A is grea8ter");
+        }else
+         {
+            System.out.println("A is lesser");
+         }
+            
+        }
+    }
+
