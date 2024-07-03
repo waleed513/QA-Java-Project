7747206330cfe383e96a08a8b903790a4f4dd729
waleed513 <waleedahmad.aj@gmail.com>
2024-07-03 14:41:54 +0500

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class loop1 {
+    public static void main(String[] args) {
+        // Example: Print numbers from 1 to 5 //
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
